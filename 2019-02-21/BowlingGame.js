915e4947ece3876e6083ea38fd4223c2aa8cffd8
Nikola Divić <divicnikola@gmail.com>
2019-02-21 17:46:27 +0100

    First kata, a tough one
    Tried to solve it a couple of times by myself, had some problems,
    took a couple of looks at Uncle Bob's code and tweaked until it started working.
    Didn't really understand the "business logic" that well, since I bowled only a
    couple of times in my life.

--- 2019-02-21/BowlingGame.js
+++ 2019-02-21/BowlingGame.js
@@ -1,1 +1,48 @@
-export class BowlingGame {}
+export class BowlingGame {
+  constructor() {
+    this.rolls = Array(21).fill(0);
+    this.currentRoll = 0;
+  }
+
+  roll(droppedPins) {
+    this.rolls[this.currentRoll++] = droppedPins;
+  }
+
+  isSpare(rollIndex) {
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1] == 10;
+  }
+
+  isStrike(rollIndex) {
+    return this.rolls[rollIndex] == 10;
+  }
+
+  sumOfPinsInFrame(rollIndex) {
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+  }
+
+  strikeBonus(rollIndex) {
+    return this.sumOfPinsInFrame(rollIndex + 1);
+  }
+
+  spareBonus(rollIndex) {
+    return this.rolls[rollIndex + 2];
+  }
+
+  score() {
+    let score = 0;
+    let frameIndex = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.isStrike(frameIndex)) {
+        score += 10 + this.strikeBonus(frameIndex);
+        frameIndex++;
+      } else if (this.isSpare(frameIndex)) {
+        score += 10 + this.spareBonus(frameIndex);
+        frameIndex += 2;
+      } else {
+        score += this.sumOfPinsInFrame(frameIndex);
+        frameIndex += 2;
+      }
+    }
+    return score;
+  }
+}
--- 2019-02-21/BowlingGame.test.js
+++ 2019-02-21/BowlingGame.test.js
@@ -1,7 +1,43 @@
 import { BowlingGame } from "./BowlingGame";
 
 describe("Bowling game", () => {
-  it("Gutter game (0 pins hit) score() should return 0", () => {
-    let game = new BowlingGame();
+  let game;
+  beforeEach(() => {
+    game = new BowlingGame();
+  });
+
+  it("should return 0 if it's a gutter game", () => {
+    rollMany(game, 20, 0);
+    expect(game.score()).toBe(0);
+  });
+  it("should return 20 if we roll all 1's", () => {
+    rollMany(game, 20, 1);
+    expect(game.score()).toBe(20);
+  });
+  it("should take spares into account", () => {
+    rollSpare(game, 3);
+    rollMany(game, 17, 0);
+    expect(game.score()).toBe(16);
+  });
+  it("should take strikes into account", () => {
+    game.roll(10);
+    game.roll(3);
+    game.roll(4);
+    expect(game.score()).toBe(24);
+  });
+  it("should return 300 for a perfect game", () => {
+    rollMany(game, 12, 10);
+    expect(game.score()).toBe(300);
   });
 });
+
+function rollSpare(game, bonus) {
+  rollMany(game, 2, 5);
+  game.roll(bonus);
+}
+
+function rollMany(game, howManyRolls, droppedPins) {
+  for (let rollCount = 0; rollCount < howManyRolls; rollCount++) {
+    game.roll(droppedPins);
+  }
+}
