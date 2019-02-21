f68e424ebca7ca3eb45704fe5e272a4b51b47d6f
Nikola Divić <divicnikola@gmail.com>
2019-02-21 15:28:28 +0100

    Setup Jest and Babel

---
+++ 2019-02-21/BowlingGame.test.js
@@ -0,0 +1,7 @@
+import { BowlingGame } from "./BowlingGame";
+
+describe("Bowling game", () => {
+  it("Gutter game (0 pins hit) score() should return 0", () => {
+    let game = new BowlingGame();
+  });
+});
--- new.sh
+++ new.sh
@@ -1,4 +1,4 @@
 #!/bin/bash          
 
 DIRNAME=$(date "+%Y-%m-%d")
-mkdir -p $DIRNAME && touch $DIRNAME/bowling.js $DIRNAME/bowling.test.js
+mkdir -p $DIRNAME && touch $DIRNAME/BowlingGame.js $DIRNAME/BowlingGame.test.js
